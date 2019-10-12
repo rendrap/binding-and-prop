3fb8f5c91e63e4c5d1ad3d33ed11cd8e56f469e3
rendrap <rendrapradana@gmail.com>
2019-10-13 06:30:09 +0700

    adding div

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 ;(function(window) {
+  // Game
   var Game = function(el, option) {
+    this.el = document.getElementById(el);
+    this.option = option;
+
+    // Info section
+    this.info_div = document.createElement('div');
+    this.info_div.id = "info_div";
+
+    // Deck
+    this.deck_div = document.createElement('div');
+    this.deck_div.id = "deck_div";
+
+    // Discard pile
+    // Rules
+    this.el.appendChild(this.info_div);
+    this.el.appendCHild(this.deck_div);
+
+
 
   }
 
+  window.Game = Game;
 })(window);
